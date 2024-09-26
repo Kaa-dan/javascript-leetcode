35891c1b00ded86d7a1da2a543d2aa9cf6e9037d
kaa-dan <nithinraj.dev@gmail.com>
2024-09-26 12:02:23 +0530

    question2

---
+++ question2.js
@@ -0,0 +1,57 @@
+// 2. Add Two Numbers
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode();
+  let curr = dummy;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || carry > 0) {
+    const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+    curr.next = new ListNode(sum % 10);
+    carry = Math.floor(sum / 10);
+    curr = curr.next;
+
+    l1 = l1?.next || null;
+    l2 = l2?.next || null;
+  }
+
+  return dummy.next;
+};
+
+//optimised answer
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode();
+  let curr = dummy;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+    const x = l1?.val ?? 0;
+    const y = l2?.val ?? 0;
+
+    curr.next = new ListNode((x + y + carry) % 10);
+    //bitwise operation is not efficient
+    carry = ~~((x + y + carry) / 10);
+    curr = curr.next;
+
+    l1 = l1?.next;
+    l2 = l2?.next;
+  }
+
+  return dummy.next;
+};
