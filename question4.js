f594e13bc251050c0cd125bca49bdc18934abc5b
kaa-dan <nithinraj.dev@gmail.com>
2024-10-07 22:33:59 +0530

    question4

--- question4.js
+++ question4.js
@@ -2,8 +2,6 @@
 
 // The overall run time complexity should be O(log (m+n)).
 
- 
-
 // Example 1:
 
 // Input: nums1 = [1,3], nums2 = [2]
@@ -15,4 +13,40 @@
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 
+// my logic its not good
+var findMedianSortedArrays = function (nums1, nums2) {
+  const newArray = [];
+
+  while (nums1.length > 0 || nums2.length > 0) {
+    if (nums1.length > 0 && nums2.length > 0) {
+      // Compare the first elements and shift the smaller one
+      if (nums1[0] > nums2[0]) {
+        newArray.push(nums2.shift());
+      } else {
+        newArray.push(nums1.shift());
+      }
+    } else {
+      if (nums1.length > 0) {
+        newArray.push(...nums1);
+        break;
+      } else {
+        newArray.push(...nums2);
+        break;
+      }
+    }
+  }
+
+  return (
+    (newArray[Math.floor(newArray.length / 2)] +
+      newArray[Math.floor((newArray.length - 1) / 2)]) /
+    2
+  );
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
