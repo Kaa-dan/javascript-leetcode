23ee41035c34fed0a7fbe6a4e5f5d8f30b4f8920
kaadan <nithinraj.dev@gmail.com>
2025-01-02 12:35:31 +0530

    learning recursive approach on question 2

--- question2.js
+++ question2.js
@@ -16,29 +16,6 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-  let dummy = new ListNode();
-
-  let curr = dummy;
-
-  let carry = 0;
-
-  while (l1 !== null || l2 !== null || carry > 0) {
-    const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
-
-    curr.next = new ListNode(sum % 10);
-
-    carry = Math.floor(sum / 10);
-
-    curr = curr.next;
-
-    l1 = l1?.next || null;
-
-    l2 = l2?.next || null;
-  }
-
-  return dummy.next;
-};
 
 //optimised answer
 var addTwoNumbers = function (l1, l2) {
